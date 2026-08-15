4256e3465912cb1a623f87bc33cc54a4c55700c6
Olibbbb <jakecanagasuriam@gmail.com>
2026-08-15 19:04:31 +0100

    player class start + small UI start

---
+++ scripts/Classes/player_class.gd
@@ -0,0 +1,15 @@
+class Player:
+	#player variables
+	var money: float = 0
+	var bridge_level: int = 1
+	var car_rate_per_second: int = 0
+	var toll_price: float = 0
+	
+	#player functions
+	func generate_money():
+		pass
+		
+	func calculate_car_rate():
+		#function to use toll price + level etc to calc how many cars
+		pass 
+	
